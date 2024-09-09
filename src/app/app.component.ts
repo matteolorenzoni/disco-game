db091547f908f0e09e61cb3be82351b45fca5d87
Matteo Lorenzoni <matteolorenzonidev@gmail.com>
2024-09-09 19:34:06 +0200

    ♻️ | Aggiunto firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirebaseService } from './service/firebase.service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  /* Services */
+  firebaseService = inject(FirebaseService);
+
+  /* Variables */
   title = 'app-base';
 }
