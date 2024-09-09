fcaa4c8a3e6982009918be20186a7ffb41cdaff2
Matteo Lorenzoni <matteolorenzonidev@gmail.com>
2024-09-09 19:45:35 +0200

    🖥️ | Fork

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'app-base' title`, () => {
+  it(`should have the 'disco-game' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app-base');
+    expect(app.title).toEqual('disco-game');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, app-base');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, disco-game');
   });
 });
