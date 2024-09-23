a50e32f773a52a9c62ff9bb22ba76091ea1d2a05
Matteo Lorenzoni <matteolorenzonidev@gmail.com>
2024-09-24 00:06:51 +0200

    🖥️ | Components | FvHeaderMenu | Creato componente

---
+++ src/app/components/fv-header-menu.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, output } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'fv-header-menu',
+  standalone: true,
+  imports: [CommonModule, FaIconComponent],
+  template: ` <nav class="flex h-14 items-center justify-between overflow-hidden bg-primary p-3 text-white">
+    <div class="flex h-full items-center gap-8">
+      <button (click)="closeMenuEvt.emit($event)"><fa-icon [icon]="ICON_MENU"></fa-icon></button>
+      <a class="flex" href="/user/dashboard">
+        <span class="self-center whitespace-nowrap text-xl font-semibold">Fanta volta</span>
+      </a>
+    </div>
+  </nav>`,
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FvHeaderMenuComponent {
+  /* Outputs */
+  closeMenuEvt = output<Event>();
+
+  /* Icons */
+  ICON_MENU = faBars;
+}
