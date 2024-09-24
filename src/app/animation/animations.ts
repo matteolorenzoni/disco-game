c09d978373fcb8a768239f2907fe61c0e5574ad8
Matteo Lorenzoni <matteolorenzonidev@gmail.com>
2024-09-24 19:22:24 +0200

    🖥️ | Components | FvSidebar | Creato componente

--- src/app/animation/animations.ts
+++ src/app/animation/animations.ts
@@ -6,3 +6,10 @@ export const loginFormAnimation = trigger('loginFormAnimation', [
   state('center', style({ transform: 'translateY(-100vh)' })),
   transition('bottom <=> center', [animate('1200ms ease-in-out')])
 ]);
+
+// Sidebar
+export const sideMenuAnimation = trigger('sideMenuAnimation', [
+  state('hidden', style({ transform: 'translateX(-100%)' })),
+  state('visible', style({ transform: 'translateX(0)' })),
+  transition('hidden <=> visible', [animate('300ms ease-in-out')])
+]);
