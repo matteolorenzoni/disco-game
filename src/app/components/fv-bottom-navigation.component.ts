bbb90eae36dccf084030c37b5fe26e3d3e78071f
Matteo Lorenzoni <matteolorenzonidev@gmail.com>
2024-10-01 22:58:27 +0200

    🖥️ | Components | FvBottomNavigation | Aggiunto style

--- src/app/components/fv-bottom-navigation.component.ts
+++ src/app/components/fv-bottom-navigation.component.ts
@@ -9,11 +9,14 @@ import { RouterLinkActive, RouterModule } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterModule, RouterLinkActive, FaIconComponent],
   template: `
-    <nav class="fixed bottom-0 left-0 w-full bg-background p-2">
-      <ul class="flex gap-2 rounded-lg bg-surface p-2 text-gray-400">
+    <nav class="fixed bottom-0 left-0 h-16 w-full bg-surface p-2 text-gray-400">
+      <ul class="flex h-full items-center gap-2">
         @for (item of menu(); track item.id) {
           <li class="grow">
-            <a [routerLink]="item.path" routerLinkActive="text-white" class="flex flex-col items-center gap-1">
+            <a
+              [routerLink]="item.path"
+              routerLinkActive="text-white"
+              class="flex flex-col items-center justify-between">
               <fa-icon class="text-sm" [icon]="item.icon"></fa-icon>
               <span class="text-xxs">{{ item.label }}</span>
             </a>
