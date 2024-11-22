56614675f7fd00a6f9e8d9a04d31f9236e5573d1
Matteo Lorenzoni <matteolorenzonidev@gmail.com>
2024-11-22 18:30:53 +0100

    🖥️ | Page | Dashboard | Aggiunto numero iscritti

--- src/app/page/admin/dashboard/dashboard.component.ts
+++ src/app/page/admin/dashboard/dashboard.component.ts
@@ -1,12 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { TitleComponent } from '../../../components/title/title.component';
+import { LoaderService } from '../../../service/loader.service';
+import { UserService } from '../../../service/user.service';
+import { Event } from '../../../model/event.model';
+import { Doc } from '../../../model/firebase';
+import { EventService } from '../../../service/event.service';
+import { TeamService } from '../../../service/team.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  /* Services */
+  private readonly userService = inject(UserService);
+  private readonly eventService = inject(EventService);
+  private readonly teamService = inject(TeamService);
+  private readonly loaderService = inject(LoaderService);
+
+  /* Variables */
+  usersCount = signal<number>(0);
+  eventTeamsCounts = signal<{ event: Doc<Event>; count: number }[]>([]);
+
+  /* -------------------- Lifecycle hooks -------------------- */
+  async ngOnInit(): Promise<void> {
+    await this.initHttp();
+  }
+
+  /* -------------------------- Methods initialization --------------------------  */
+  private async initHttp() {
+    await this.loaderService.executeImmediate(async () => {
+      const usersCount = await this.userService.getUsersCount();
+      this.usersCount.set(usersCount);
+
+      const events = await this.eventService.getAllEvents();
+      events.forEach(async (event) => {
+        const teamsCount = await this.teamService.getUsersCountByEventId(event.id);
+        this.eventTeamsCounts.update((val) => [...val, { event, count: teamsCount }]);
+      });
+    });
+  }
+}
